aa993447cfa40248018a8010b9ea14b84db52563
Rodrigo Daros <rodrigo.conspena@gmail.com>
2017-05-29 15:32:23 -0300

    - Commit inicial.

---
+++ Unity/Assets/Script/Coolerman.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class Coolerman : MonoBehaviour
+{
+    #region Constantes
+
+    #endregion Constantes
+
+    #region Atributos
+
+    #endregion Atributos
+
+    #region Construtores
+
+    #endregion Construtores
+
+    #region Métodos
+
+    private void iniciar()
+    {
+        this.iniciarPosicao();
+    }
+
+    private void iniciarPosicao()
+    {
+        Vector3 vctCentro = new Vector3(0, .5f, 0);
+
+        Vector3 vctPosicao = this.randomizarCircle(vctCentro, 5.0f);
+
+        Quaternion qtnRotacao = Quaternion.FromToRotation(Vector3.forward, vctCentro - vctPosicao);
+
+        this.transform.position = vctPosicao;
+        this.transform.rotation = qtnRotacao;
+    }
+
+    private Vector3 randomizarCircle(Vector3 vctCentro, float fltRadius)
+    {
+        float fltAngulo = (Random.value * 360);
+
+        Vector3 vctPosicao = new Vector3(0, .5f, 0);
+
+        vctPosicao.x = (vctCentro.x + fltRadius * Mathf.Sin(fltAngulo * Mathf.Deg2Rad));
+        vctPosicao.z = (vctCentro.z + fltRadius * Mathf.Cos(fltAngulo * Mathf.Deg2Rad));
+
+        return vctPosicao;
+    }
+
+    #endregion Métodos
+
+    #region Eventos
+
+    public void Start()
+    {
+        this.iniciar();
+    }
+
+    public void Update()
+    {
+    }
+
+    #endregion Eventos
+}
---
+++ Unity/Assets/Script/CoolermanManger.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using UnityEngine;
+
+public class CoolermanManger : MonoBehaviour
+{
+    #region Constantes
+
+    #endregion Constantes
+
+    #region Atributos
+
+    private DateTime _dttUltimo;
+    private int _intQuantidadeAtual = 0;
+    private int _intQuantidadeLimite = 5;
+    private int _intTempoNascimento = 250;
+    private GameObject _objCoolerMan;
+
+    private DateTime dttUltimo
+    {
+        get
+        {
+            return _dttUltimo;
+        }
+
+        set
+        {
+            _dttUltimo = value;
+        }
+    }
+
+    private int intQuantidadeAtual
+    {
+        get
+        {
+            return _intQuantidadeAtual;
+        }
+
+        set
+        {
+            _intQuantidadeAtual = value;
+        }
+    }
+
+    private int intQuantidadeLimite
+    {
+        get
+        {
+            return _intQuantidadeLimite;
+        }
+
+        set
+        {
+            _intQuantidadeLimite = value;
+        }
+    }
+
+    private int intTempoNascimento
+    {
+        get
+        {
+            return _intTempoNascimento;
+        }
+
+        set
+        {
+            _intTempoNascimento = value;
+        }
+    }
+
+    private GameObject objCoolerMan
+    {
+        get
+        {
+            if (_objCoolerMan != null)
+            {
+                return _objCoolerMan;
+            }
+
+            _objCoolerMan = Resources.Load<GameObject>("Prefab/Coolerman");
+
+            return _objCoolerMan;
+        }
+    }
+
+    #endregion Atributos
+
+    #region Construtores
+
+    #endregion Construtores
+
+    #region Métodos
+
+    private void gerarCoolerman()
+    {
+        if (this.intQuantidadeAtual >= this.intQuantidadeLimite)
+        {
+            return;
+        }
+
+        var intDiff = (DateTime.Now - this.dttUltimo).Milliseconds;
+
+        if ((DateTime.Now - this.dttUltimo).Milliseconds < this.intTempoNascimento)
+        {
+            return;
+        }
+
+        this.dttUltimo = DateTime.Now;
+        this.intQuantidadeAtual++;
+
+        Instantiate(this.objCoolerMan);
+    }
+
+    #endregion Métodos
+
+    #region Eventos
+
+    public void Start()
+    {
+        this.dttUltimo = DateTime.Now;
+    }
+
+    public void Update()
+    {
+        this.gerarCoolerman();
+    }
+
+    #endregion Eventos
+}
